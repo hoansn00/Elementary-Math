a703619b0ac67d65013b74393741d0af87b209a5
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2020-11-09 20:21:43 +0700

    index

---
+++ resources/views/fontend/home/index.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elementary Math</title>
+    <link rel="stylesheet" href="fontend/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="top-bar">
+            <div class="top-bar-btn">
+                <a class="login" href="">Đăng ký</a>
+                <a class="register" href="">Đăng nhập</a>
+            </div>
+        </div>
+        <div class="main-header">
+            <div class="container">
+                <a href=""><img src="fontend/images/logo.png" alt="" height="100px"></a>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="">Trang chủ</a>
+                        </li>
+                        <li>
+                            <a href="">Giới thiệu</a>
+                        </li>
+                        <li>
+                            <a href="">Học toán</a>
+                        </li>
+                        <li>
+                            <a href="">Tài liệu</a>
+                        </li>
+                        <li>
+                            <a href="">Kiểm tra</a>
+                        </li>
+                        <li>
+                            <a href="">Hướng dẫn</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <slider>  
+    <div class="banner">
+    <img src="fontend/images/banner.png" alt="" width="100%" height="450px">
+    </div>
+    </slider>
+    <main>
+        <div class="grade container">
+            <a href="" class="class-one">
+                <span >Lớp 1</span>
+            </a>
+            <a href="" class="class-two">
+                <span >Lớp 2</span>
+            </a>
+            <a href="" class="class-three">
+                <span >Lớp 3</span>
+            </a>
+        </div>
+        <div class="grade container">
+            <a href="" class="class-four">
+                <span >Lớp 4</span>
+            </a>
+            <a href="" class="class-five">
+                <span >Lớp 5</span>
+            </a>
+            <a href="" class="test">
+                <span >Luyện thi</span>
+            </a>
+        </div>
+    </main>
+    <introduction>
+        <div class="container">
+            <div class="description">
+                <p>"Elementary Math là website học toán online hiệu quả với hàng ngàn video bài giảng, bài tập luyện tập từ cơ bản đến nâng cao.
+                    Chúng tôi luôn nỗ lực không ngừng để mang đến cho học sinh những bài học sinh động, lý thú, giúp các em vững vàng kiến thức 
+                    và say mê, yêu thích môn Toán hơn. Elementary Math là tình yêu, đam mê và tâm huyết của tôi."
+                </p>
+                <div class="group">
+                    <div>
+                    <img src="fontend/images/boss.jpg" width="100px" alt="">
+                    <p>Phạm Văn Hoan</p>
+                    </div>
+                    <div>
+                    <img src="fontend/images/boss2.jpg" width="100px" alt="">
+                    <p>Vũ Minh Hiếu</p>
+                    </div>
+                </div>
+            </div>
+            <div class="video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/L3KTOT0TO60?start=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+            </div>
+        </div>
+    </introduction>
+</body>
+</html>
